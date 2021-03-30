fffcb0d5f94649b6c100b5e93c405aaccda61696
Magnus Appel <68828420+magnuss86@users.noreply.github.com>
2021-03-30 10:41:30 -0700

    text update

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -15,7 +15,7 @@ class Forms extends React.Component {
 render (){
     return (
         <div className="form-row">
-           <Form.Control type="text" placeholder="Enter ZipCode here" />
+           <Form.Control type="text" placeholder="ZipCode here" />
         </div> 
             );
 }
